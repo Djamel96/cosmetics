5f85c5ef22630f8dfc83d0bca6fad7b9b9f4eca1
djamel96 <djamelmadani3@gmail.com>
2023-04-04 12:45:35 +0100

    d

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "fd896f643a1a3e07f6c35f73bba890d9",
-"index.html": "197c3408526b9468b94e95a909b109cf",
-"/": "197c3408526b9468b94e95a909b109cf",
-"main.dart.js": "9b5527079ba5ddbf618d4091977de536",
+"index.html": "a83399049bb175a534eca1e3ee14ced4",
+"/": "a83399049bb175a534eca1e3ee14ced4",
+"main.dart.js": "ee943812aebbc4c18263f65e8ed3199f",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
